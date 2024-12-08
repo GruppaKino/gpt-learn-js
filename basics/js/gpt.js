9d55b5a7dd7bee8bb61c37a96a6c8c70389049f2
k1no <veredyukov30@gmail.com>
2024-12-08 13:25:30 +0300

    Не большая тренировка

--- basics/js/gpt.js
+++ basics/js/gpt.js
@@ -2,3 +2,11 @@ const myName = 'Владимир';
 let myAge = 24;
 const isLearningJavaScript = true;
 console.log(`Мое имя ${myName}. Мне ${myAge}. Я хочу выучить JavaScript и это ${isLearningJavaScript}`);
+
+if (age < 18) {
+    console.log('Ты еще пиздюк');
+} else if (age === 18) {
+    console.log('Тебе только исполнилось 18')
+} else {
+    console.log('Ты уже бугай!')
+}
