c445b11bf4cc2d18c86fa52f2fc2bf18eb702ab1
k1no <veredyukov30@gmail.com>
2024-12-08 13:15:10 +0300

    first commit

---
+++ basics/js/gpt.js
@@ -0,0 +1,4 @@
+const myName = 'Владимир';
+let myAge = 24;
+const isLearningJavaScript = true;
+console.log(`Мое имя ${myName}. Мне ${myAge}. Я хочу выучить JavaScript и это ${isLearningJavaScript}`);
